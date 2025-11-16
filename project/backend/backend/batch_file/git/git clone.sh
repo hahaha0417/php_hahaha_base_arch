66f98fb4fd9ceeed70cefffc498f99c6ac229a1f
hahaha <hahaha0417@hotmail.com>
2025-11-16 17:41:46 +0800

    fix

--- project/backend/backend/batch_file/git/git clone.sh
+++ project/backend/backend/batch_file/git/git clone.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
 # pwd
-mkdir ../../library
-cd ../../library
 
-git clone git@github.com:hahaha0417/php_hahahalib.git
-git clone php_hahahalib_hard_license
-git clone php_hahahalib_very_hard_license
+# --------------------------------------------
+# hahahalib
+# --------------------------------------------
+cd ../git/hahahalib
+./'git clone.sh'
+cd ..
+# --------------------------------------------
+
+# --------------------------------------------
+# php_hahahalib_hard_license
+# --------------------------------------------
+cd ../git/hahahalib_hard_license
+./'git clone.sh'
+cd ..
+# --------------------------------------------
+
+# --------------------------------------------
+# php_hahahalib_hard_license
+# --------------------------------------------
+cd ../git/hahahalib_very_hard_license
+./'git clone.sh'
+cd ..
+# --------------------------------------------
+
 
 # read
